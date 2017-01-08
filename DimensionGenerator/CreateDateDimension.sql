860022d4ac55f802d2a72aacacb35d926ae9342b
Steve Jost <steve@stevejost.com>
2017-01-07 22:41:47 -0800

    Created readme, added SQL script for generating table.

---
+++ DimensionGenerator/CreateDateDimension.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[DateDimension](
+	[DateId] [datetime] NOT NULL,
+	[DateName] [varchar](25) NULL,
+	[DateNameAbbrev] [varchar](25) NULL,
+	[DayOfYear] [int] NULL,
+	[CalendarYearBegin] [datetime] NULL,
+	[CalendarYearEnd] [datetime] NULL,
+	[CalendarYear] [int] NULL,
+	[CalendarMonthBegin] [datetime] NULL,
+	[CalendarMonthEnd] [datetime] NULL,
+	[CalendarMonthNumber] [int] NULL,
+	[CalendarMonthName] [varchar](25) NULL,
+	[CalendarMonthNameAbbrev] [varchar](25) NULL,
+	[FiscalYear] [int] NULL,
+	[FiscalYearBegin] [datetime] NULL,
+	[FiscalYearEnd] [datetime] NULL,
+	[WeekBeginDate] [datetime] NULL,
+	[WeekEndDate] [datetime] NULL,
+	[IsWeekend] [bit] NULL,
+ CONSTRAINT [PK_DateDimension] PRIMARY KEY CLUSTERED 
+(
+	[DateId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
---
+++ README.md
@@ -0,0 +1,9 @@
+# Date Dimension Generator #
+
+This is a quick utility to generate date dimension tables.  
+
+Currently will only insert into a table called DateDimension.  
+
+Run CreateDateDimension SQL procedure before utilizing.
+
+
